a1918cb4dde4075375f72b1121d1297b75f1d0eb
Raul Rivero <raul9501@gmail.com>
2016-08-08 16:42:46 -0400

    Create binarysearch.js

---
+++ binarysearch.js
@@ -0,0 +1,17 @@
+function binaryIndexOf(searchElement) {
+    var minIndex = 0, maxIndex = this.length - 1, currentIndex;
+    while (minIndex <= maxIndex) {
+    	//num | 0 is the same as Math.floor(num) but a bit faster
+        currentIndex = (minIndex + maxIndex) / 2 | 0;
+        if (this[currentIndex] < searchElement) {
+            minIndex = currentIndex + 1;
+        }
+        else if (this[currentIndex] > searchElement) {
+            maxIndex = currentIndex - 1;
+        }
+        else {
+            return currentIndex;
+        }
+    }
+    return -1;
+}
