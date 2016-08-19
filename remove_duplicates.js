9588911e022864ab9cd1bf43b75134469f8cc4ba
Raul Rivero <raul9501@gmail.com>
2016-08-19 15:54:47 -0400

    Update remove_duplicates.js

--- remove_duplicates.js
+++ remove_duplicates.js
@@ -1,4 +1,4 @@
-//Remove duplicates from an array, fastest way possible, 
+//Remove duplicates from an array, this is the fastest way possible, 
 //time complexity O(n), space complexity O(n - d) where d is number of duplicate elements
 function removeDuplicates(arr) {
 	var seen = {}, res = [], len = arr.length, j = 0, i = 0;
