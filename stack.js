eb485f66d8b1427e32747f0dd495659ee00c9453
Raul Rivero <raul9501@gmail.com>
2016-08-28 11:38:19 -0400

    Update stack.js

--- stack.js
+++ stack.js
@@ -8,7 +8,7 @@ Stack.prototype.pop = function () {
 	return this.data.pop();
 };
 Stack.prototype.top = function () {
-	return this.data[0];
+	return this.data[this.data.length - 1];
 };
 Stack.prototype.isEmpty = function () {
 	return this.data.length === 0;
