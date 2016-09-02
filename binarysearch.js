bca20bb578c3428aaadfe6be1633a869a31ff60b
Raul Rivero <raul9501@gmail.com>
2016-09-01 23:04:19 -0400

    Update binarysearch.js

--- binarysearch.js
+++ binarysearch.js
@@ -1,5 +1,5 @@
 //Function added to native's Array prototype, returns the index of the element we're looking for
-Array.prototype.binarySearch = function (searchElement) {
+function binarySearch(searchElement) {
     var minIndex = 0, maxIndex = this.length - 1, currentIndex;
     while (minIndex <= maxIndex) {
     	//num | 0 is the same as Math.floor(num) but a bit faster
@@ -15,4 +15,26 @@ Array.prototype.binarySearch = function (searchElement) {
         }
     }
     return -1;
-};
+}
+
+var sorted = [1, 3, 5, 6, 7, 23, 33, 34, 55, 69];
+binarySearch.call(sorted, 69);
+
+// Recursive implementation of the Binary Search algorithm
+function binarySearch(sortedArray, elem, start, end) {
+	var half = (start + end) / 2 | 0;
+	if (start > end) {
+		return -1;
+	}
+	// search on the right half
+	if (sortedArray[half] < elem) {
+		return binarySearch(sortedArray, elem, half + 1, end);
+	}
+	// search on the left half
+	if (sortedArray[half] > elem) {
+		return binarySearch(sortedArray, elem, 0, half - 1);
+	}
+	return half;
+}
+
+binarySearch(sorted, 69, 0, sorted.length-1);
