7833c703b88c557c504933f23737bc325a148726
Raul Rivero <raul9501@gmail.com>
2016-09-06 12:22:06 -0400

    Create priority-queue.js

---
+++ priority-queue.js
@@ -0,0 +1,19 @@
+function PriorityQueue () {
+    this._nodes = [];
+
+    this.enqueue = function (priority, key) {
+        this._nodes.push({key: key, priority: priority });
+        this.sort();
+    }
+    this.dequeue = function () {
+        return this._nodes.shift().key;
+    }
+    this.sort = function () {
+        this._nodes.sort(function (a, b) {
+            return a.priority - b.priority;
+        });
+    }
+    this.isEmpty = function () {
+        return !this._nodes.length;
+    }
+}
