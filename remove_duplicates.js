96d2f54ab6897df0d20cabcc93f2ec0b2c419735
Raul Rivero <raul9501@gmail.com>
2017-05-08 14:15:54 -0400

    added new way using javascript ES6

--- remove_duplicates.js
+++ remove_duplicates.js
@@ -1,12 +1,20 @@
 //Remove duplicates from an array, this is the fastest way possible, 
 //time complexity O(n), space complexity O(n - d) where d is number of duplicate elements
 function removeDuplicates(arr) {
-	var seen = {}, res = [], len = arr.length, j = 0, i = 0;
-    for (; i < len; ++i) {
-		if(!seen[arr[i]]) {
-	 		seen[arr[i]] = true;
-			res[j++] = arr[i];
-		}
-    }
-    return res;
+	let seen = {}, res = [], len = arr.length, j = 0, i = 0;
+	if (Array.isArray(arr)) {
+	  for (; i < len; ++i) {
+		  if(!seen[arr[i]]) {
+	 		  seen[arr[i]] = true;
+			  res[j++] = arr[i];
+		  }
+    }  
+	}
+  return res;
+}
+
+// Using ES6 Set object which creates a new object with unique values of any type and then converting that to an array with 
+// Array.from()
+function removeDuplicates(a) {
+	return Array.from(new Set(a));
 }
