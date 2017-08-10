9207a0415f8825bafaf26b48cfbd14eb5232a5eb
Raul Rivero <raul9501@gmail.com>
2017-08-10 15:40:10 -0400

    Update stack.js

--- stack.js
+++ stack.js
@@ -1,15 +1,40 @@
-function Stack () {
-	this.data = [];
+class Stack {
+  constructor() {
+    this.data = [];
+    this._length = 0;
+  }
+  
+  push(value) {
+    this.data.push(value);
+    this._length++;
+  }
+  
+  top() {
+    return !this.isEmpty() ? this.data[this.data.length - 1] : null;  
+  }
+  
+  pop() {
+    const topElement = this.data.pop();
+    this._length = !this.isEmpty() ? this._length - 1 : 0;
+    return topElement;
+  } 
+  
+  isEmpty() {
+	  return this._length === 0;
+  }
+  
+  size() {
+    return this._length;
+  }
 }
-Stack.prototype.push = function (val) {
-	this.data.push(val);
-};
-Stack.prototype.pop = function () {
-	return this.data.pop();
-};
-Stack.prototype.top = function () {
-	return this.data[this.data.length - 1];
-};
-Stack.prototype.isEmpty = function () {
-	return this.data.length === 0;
-};
+
+// How to use it
+let stack = new Stack();
+stack.push(1);
+stack.push(2);
+stack.push(3);
+stack.push(4);
+stack.push(5);
+
+console.log(stack.pop());
+console.log(stack.size());
