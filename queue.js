65ed5c71a7329d0219d81134f14056b0aef44857
Raul Rivero <raul9501@gmail.com>
2017-09-23 12:10:27 -0400

    Update queue.js

--- queue.js
+++ queue.js
@@ -1,18 +1,20 @@
-function Queue () {
-	this.data = [];
+class Queue {
+  constructor() {
+    this.data = [];  
+  }
+	enqueue(val) {
+		this.data.push(val);  
+	}
+	dequeue() {
+		return this.data.shift();  
+	}
+	peek() {
+		return this.data[0];  
+	}
+  isEmpty() {
+  	return this.data.length === 0;
+  }
+  clear() {
+	  this.data = [];
+  }
 }
-Queue.prototype.enqueue = function (val) {
-	this.data.push(val);
-};
-Queue.prototype.dequeue = function () {
-	return this.data.shift();
-};
-Queue.prototype.peek = function () {
-	return this.data[0];
-};
-Queue.prototype.isEmpty = function () {
-	return this.data.length === 0;
-};
-Queue.prototype.clear = function () {
-	this.data = [];
-};
