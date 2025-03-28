50da5c0eca9259c9334a6153169ccfedf0dee04c
Raul Rivero <raul9501@gmail.com>
2025-03-28 06:01:40 -0700

    Update flattenarrays.js

--- flattenarrays.js
+++ flattenarrays.js
@@ -1,21 +1,28 @@
-//Using the Array.prototype.reduce function of javascript 
+// Using Array.prototype.reduce and recursion
 function flattenArray(arr) {
     return arr.reduce(function (flat, toFlatten) {
         return flat.concat(Array.isArray(toFlatten) ? flattenArray(toFlatten) : toFlatten);
     }, []);
 }
 
-//Using just recursion and regular loops
+// Using recursion and regular loops
 function flattenArray(array, res) {
-	for (var i = 0, len = array.length; i < len; ++i) {
-		if (Array.isArray(array[i])) {
-			flattenArray(array[i], res);
-		}
-		else {
-			res.push(array[i]);
-		}
-	}
-	return res;
+  for (var i = 0, len = array.length; i < len; ++i) {
+	  if (Array.isArray(array[i])) {
+		  flattenArray(array[i], res);
+	  } else {
+		  res.push(array[i]);
+	  }
+  }
+  return res;
+}
+
+// Using Array.prototype.some without recursion
+function flattenArray(array) {
+	while (array.some(Array.isArray)) {
+   		array = [].concat(...array);
+  	}
+  	return array;
 }
 
 flattenArray([1, 2, [3, 4, [5]], 6], []); // [1, 2, 3, 4, 5, 6]
